8bf4f622c71c86360324159667ba94de2edf9ac4
PewhProgrammer <vlukiray@yahoo.com>
2016-07-30 18:52:37 +0200

    conventionalized function names and added try/except handler for I/O error from user

--- Prog2_PointsCalculator/calc.py
+++ Prog2_PointsCalculator/calc.py
@@ -1,5 +1,5 @@
 def max(x, y):
-    return (x,y)[x < y]
+    return (x, y)[x < y]
 
 
 def min(x, y):
@@ -8,51 +8,66 @@ def min(x, y):
     return x
 
 
-def calcK(max, x):
+def calc_k(max, x):
     return max / x
 
 
-def calcQ(k, p):
+def calc_q(k, p):
     return 0.5 * k + 0.5 * p
 
 
-def calcMinimum(p):
+def calc_minimum(p):
     return (0.5 - (0.5 * p)) / 0.5
 
+
 def interpolate_q(q):
-    #map 1-0.5 to 1-4
+    # map 1-0.5 to 1-4
     return (((q - 1) * (4 - 1)) / (0.5 - 1)) + 1
 
 
-def Opt1():
-    x = float(input("Insert Points in main exam:\t"))
-    y = float(input("Insert Points in after exam:\t"))
-    z = float(input("Insert Points in projects:\t"))
+def opt1():
+    try:
+        x = float(input("Insert Points in main exam:\t"))
+        y = float(input("Insert Points in after exam:\t"))
+        z = float(input("Insert Points in projects:\t"))
+    except ValueError:
+        print("Error in Input. Cancelling!")
+        exit()
 
-    k = calcK(max(x, y), 100)
+    k = calc_k(max(x, y), 100)
     p = min(z, 100) / 100
 
-    q = calcQ(k, p)
+    q = calc_q(k, p)
     grade = interpolate_q(q)
+
     if q >= 0.5:
         if k >= 0.5:
             print("you've pass with a grade of {0}!".format(grade))
+
     else:
         print("you didn't pass! Loser")
 
 
-def Opt2():
-    x = float(input("Insert Points in your projects:\t"))
+def opt2():
+    try:
+        x = float(input("Insert Points in your projects:\t"))
+    except ValueError:
+        print("Error in Input. Cancelling!")
+        exit()
+
     p = min(x, 100) / 100
 
-    print("you'll need a minimum {0} percentage".format(max(calcMinimum(p) * 100, 50)))
+    print("you'll need a minimum {0} percentage".format(max(calc_minimum(p) * 100, 50)))
 
 
 if __name__ == "__main__":
-    if (int(input('Opt: \n'
-              '(1) Calculate your grade based of your points \n'
-              '(2) Calculate percentage needed to pass exam out of project points\n'
-              'Your Input: ')) == 1):
-        Opt1()
-    else:
-        Opt2()
+    try:
+        if (int(input('Opt: \n'
+                      '(1) Calculate your grade based of your points \n'
+                      '(2) Calculate percentage needed to pass exam out of project points\n'
+                      'Your Input: ')) == 1):
+            opt1()
+        else:
+            opt2()
+    except ValueError:
+        print("Error in Input. Cancelling!")
